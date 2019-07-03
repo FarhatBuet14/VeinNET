c13357e69f1de1881179c203318e681a3b727103
Farhat Binte Azam <33277216+FarhatBuet14@users.noreply.github.com>
2019-07-03 16:21:38 +0600

    add manually selsct points.py

--- Codes/Prepare_dataset_for_training.py
+++ Codes/Prepare_dataset_for_training.py
@@ -11,3 +11,12 @@ import imgaug as ia
 import imgaug.augmenters as iaa
 from PIL import Image
 import shutil
+
+
+data_folder = "./Data/All/"
+extraction_folder = "./Extracted/"
+troughs_folder = "./Extracted/Troughs/"
+vein_folder = "./Extracted/Vein_Images/"
+bounding_box_folder = "./Extracted/Bounding_box/"
+pos_vein_folder = "./Extracted/Vein_Images/pos_vein_img/"
+neg_vein_folder = "./Extracted/Vein_Images/neg_vein_img"
