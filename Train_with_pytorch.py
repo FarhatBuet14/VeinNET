ecc2f5f7b86825c61acd43cb2e45e94a9865a93f
Farhat Binte Azam <33277216+FarhatBuet14@users.noreply.github.com>
2019-07-12 13:20:58 +0600

    train with pytorch model

--- Train_with_pytorch.py
+++ Train_with_pytorch.py
@@ -6,6 +6,8 @@ import time
 import cv2
 from os.path import join
 from PIL import Image
+import pandas as pd
+import matplotlib as plt
 
 ########################## Import libraries for Model  ########################
 ###############################################################################
@@ -23,9 +25,8 @@ from torchvision import transforms, datasets, models
 ###############################################################################
 
 class SeedlingDataset(Dataset):
-    def __init__(self, names, labels, root_dir, subset=False, 
+    def __init__(self, labels, root_dir, subset=False, 
                  transform = None, normalize=True):
-        self.names = names
         self.labels = labels
         self.root_dir = root_dir
         self.transform = transform
@@ -36,35 +37,32 @@ class SeedlingDataset(Dataset):
         return torch.tensor(cv2.Canny(image, 50, 150))
     
     def __len__(self):
-        return self.labels.shape[0]
+        return len(self.labels)
     
-    def get_data(self):
-        for img_name in self.names:
-            fullname = join(self.root_dir, img_name)
-            image = Image.open(fullname).convert('RGB')
-            if self.transform:
-                image = self.transform(image)
-            trans = transforms.ToPILImage()
-            trans1 = transforms.ToTensor()
-            image = self.get_accumEdged(trans(trans1(image)))
-            if self.normalize:
-                image = image / 255
-            labels = torch.as_tensor(self.labels)
-            image = torch.as_tensor(image, dtype = torch.int32)
+    def __getitem__(self, idx):
+        img_name = self.labels.iloc[idx, 0]
+        fullname = join(self.root_dir, img_name)
+        image = np.array(Image.open(fullname).convert('RGB'))
+        labels = torch.tensor(self.labels.iloc[idx, 1:], 
+                            dtype = torch.float32)
+        if self.transform:
+            image = self.transform(image)
         return image, labels
 
 ############################# Define training method  #########################
 ###############################################################################
 
-def train_model(dataloaders, model, loss, optimizer, 
-                scheduler, num_epochs = 10):
+def train_model(dataloaders, model, cal_loss, optimizer, 
+                scheduler, batch_size = 2, num_epochs = 10, use_gpu = True):
     since = time.time()
-
+    train_losses = []
+    val_losses = []
     best_model_wts = model.state_dict()
-    best_acc = 0.0
+    best_loss = 100.0
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    for epoch in range(num_epochs+1):
+        print('-' * 10)
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
         print('-' * 10)
 
         # Each epoch has a training and validation phase
@@ -76,29 +74,29 @@ def train_model(dataloaders, model, loss, optimizer,
                 model.train(False)  # Set model to evaluate mode
 
             running_loss = 0.0
-            running_corrects = 0
             running_batch = 0
 
             # Iterate over data.
             for data in dataloaders[phase]:
                 # get the inputs
                 inputs, labels = data
-                labels = labels.view(-1)
+                labels = labels
                 
                 # wrap them in Variable
                 if use_gpu:
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
+                    inputs = Variable(inputs.cuda(), requires_grad = True)
+                    labels = Variable(labels.cuda(), requires_grad = True)
                 else:
-                    inputs, labels = Variable(inputs), Variable(labels)
+                    inputs = Variable(inputs, requires_grad = True)
+                    labels = Variable(labels, requires_grad = True)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
                 # forward
                 outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = loss(outputs, labels)
+                if(cal_loss.loss_type == 'mae'):
+                    loss = cal_loss.mae(labels, outputs)
 
                 # backward + optimize only if in training phase
                 if phase == 'train':
@@ -106,29 +104,41 @@ def train_model(dataloaders, model, loss, optimizer,
                     optimizer.step()
 
                 # statistics
-                running_loss += loss.data[0]
-                running_corrects += torch.sum(preds == labels.data)
+                running_loss += loss.data.item()
                 running_batch +=1
+            epoch_loss = running_loss / running_batch            
+            # Save the loss history
+            if phase == 'train': train_losses.append(epoch_loss)
+            else: val_losses.append(epoch_loss)
 
-            epoch_loss = running_loss / running_batch
-            epoch_acc = running_corrects / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+            print('{} Loss: {:.4f}'.format(phase, epoch_loss))
 
             # deep copy the model
-            if phase == 'valid' and epoch_acc > best_acc:
-                best_acc = epoch_acc
+            if phase == 'valid' and epoch_loss < best_loss:
+                best_loss = epoch_loss
                 best_model_wts = model.state_dict()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print('Best val loss: {:4f}'.format(best_loss))
 
     # load best model weights
     model.load_state_dict(best_model_wts)
-    return model
+    return model, train_losses, val_losses
+
+###########################  Define Loss Funtion ###############################
+################################################################################
+
+class cal_loss():
+    def __init__(self, loss_type):
+        self.loss_type = loss_type
+    
+    def mae(self, target, pred):
+        self.target = target
+        self.pred = pred
+        return torch.sum(torch.abs(pred - target)) / target.numel()
+
 
 ##############################  Import Data  ##################################
 ###############################################################################
@@ -157,23 +167,28 @@ y_train_aug = data['y_train_aug'].reshape((-1, 4))
 X_names = np.concatenate((X_train_names, X_train_aug_names), axis = 0)
 y = np.concatenate((y_train, y_train_aug), axis = 0)
 
-# Train Test Splitting
-random_seed = 0
-from sklearn.model_selection import train_test_split
-X_train_names, X_val_names, y_train, y_val = train_test_split(X_names, y, test_size = 0.3, 
-                                                  random_state=random_seed)
+data = []
+for index in range(0, len(X_names)):
+    data.append([X_names[index], y[index, 0], y[index, 1],
+                  y[index, 2], y[index, 3]])
 
-trans = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.ToTensor()
-])
+data_df = pd.DataFrame(data, columns=['file_name', 'point_1x', 'point_1y',
+                                        'point_2x', 'point_2y']) 
 
-train_set = SeedlingDataset(X_train_names, y_train, train_data_folder, trans, normalize=True)
-val_set = SeedlingDataset(X_val_names, y_val, train_data_folder, trans, normalize=True)
+train_data = data_df.sample(frac=0.7)
+valid_data = data_df[~data_df['file_name'].isin(train_data['file_name'])]
 
-batch_size = 32
-train_loader = DataLoader(train_set.get_data(), batch_size=batch_size, shuffle=True)
-valid_loader = DataLoader(val_set.get_data(), batch_size=batch_size, shuffle=True)
+trans = transforms.Compose([transforms.ToTensor()])
+
+train_set = SeedlingDataset(train_data, train_data_folder, 
+                            transform = trans, normalize = True)
+val_set = SeedlingDataset(valid_data, train_data_folder, 
+                          transform = trans, normalize = True)
+
+batch_size = 16
+train_loader = DataLoader(train_set, batch_size = batch_size, shuffle = True)
+valid_loader = DataLoader(val_set, batch_size = batch_size, shuffle = True)
+
 
 dataset_sizes = {
     'train': len(train_loader.dataset), 
@@ -187,21 +202,26 @@ num_pred_value = y_train.shape[1]
 ###############################################################################
 
 model = models.resnet50(pretrained=True)
-
-# training with these layers unfrozen for a couple of epochs after the initial frozen training
-for param in model.parameters():
-    param.requires_grad = False
+# model = models.resnet18(pretrained=True)
+model.conv1.in_channels = 1
 
 num_ftrs = model.fc.in_features
 model.fc = torch.nn.Linear(num_ftrs, num_pred_value)
 
-use_gpu = torch.cuda.is_available()
-if use_gpu:
-    model = model.cuda()
 
-loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.fc.parameters(), 
-                            lr=0.001, momentum=0.9)
+# # training with these layers unfrozen for a couple of epochs after the initial frozen training
+# # Freeze model parameters
+# for param in resnet50.parameters():
+#     param.requires_grad = False
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+model_loss = cal_loss(loss_type = 'mae')
+optimizer = torch.optim.RMSprop(model.fc.parameters(),lr=1e-2, 
+                                alpha=0.99, eps=1e-8, weight_decay=0, 
+                                momentum=0, centered=False)
+
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, 
                                        step_size=7,
                                        gamma=0.1)
@@ -209,4 +229,29 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer,
 
 loaders = {'train':train_loader, 'valid':valid_loader}
 
-model = train_model(loaders, model, loss, optimizer, exp_lr_scheduler, num_epochs=1)
+gpu_available = torch.cuda.is_available()
+num_epochs = 20
+model, train_losses, val_losses = train_model(loaders, model, model_loss, optimizer, 
+                    exp_lr_scheduler, batch_size, num_epochs, 
+                    gpu_available)
+
+
+torch.save(model, 'resnet50_mine.pth')
+
+plt.plot(train_losses, label='Training loss')
+plt.plot(test_losses, label='Validation loss')
+plt.legend(frameon=False)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
